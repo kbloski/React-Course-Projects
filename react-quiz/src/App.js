7fd0458a1f8b3d76f5283c96c13152e9d90956de
Kamil Błoński <kblonski02@gmail.com>
2025-01-14 03:37:49 +0100

    Add useQuestions hook

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,8 +1,10 @@
 import { useEffect, useReducer, useState } from "react";
 import Header from "./components/Header";
+import Loader from './components/Loader'
 // import data from './store/questions.json'
 
 import Main from "./components/Main";
+import { useQuestions } from "./hooks/useQuestions";
 // import DateCounter from "./components/DateCounter";
 
 const initialState = {
@@ -24,25 +26,25 @@ function reducer(state, action){
 
 function App() {
   const [state, dispatch] = useReducer( reducer, initialState)
+  const { questions , isLoading, error } = useQuestions()
 
   useEffect( () => {
-    fetch('/api/questions.json')
-    .then( res => {
-      if (!res.ok) throw new Error("Error load questions")
-
-      return res.json()
-    }).then( questions => dispatch({ type: 'setQuestions', payload: questions}))
-    .catch(err => {
-      console.error(err)
-    })
-  })
+    // console.log( questions)
+    if (!questions) return;
+    dispatch({type: 'setQuestions', payload: questions})
+  }, [questions])
+  
 
   return (
       <div className="app">
           <Header />
           <Main>
-              <p>1/15</p>
-              <p>Questions?</p>
+              { !!isLoading && <Loader />}
+              { !isLoading && <>
+                <p>1/15</p>
+                <p>Questions?</p>
+                { JSON.stringify( state.questions ) }
+              </>}
           </Main>
       </div>
   );
